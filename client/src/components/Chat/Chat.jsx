63ba528bf1ebf71cf6f12c16fbdde8a0ac32a097
Iqbal Sahrizal <54559728+iqbal2906@users.noreply.github.com>
2022-02-15 19:08:38 +0700

    setup the backend for socketIo

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+let socket;
 
 const Chat = () => {
-  return <div>Hello from chat components</div>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = new Proxy(
+      new URLSearchParams(window.location.search),
+      {
+        get: (searchParams, prop) => searchParams.get(prop),
+      }
+    );
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+
+      socket.off();
+    };
+  }, [ENDPOINT, window.location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+          type="text"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
